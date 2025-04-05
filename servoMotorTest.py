bda9291e5e5c955bd32c46aabe2c34d2c5913814
Leo Schaefer <leo.schaefer@bluewin.ch>
2025-04-05 18:19:39 +0200

    First Servo Test
    nothing more to say, its from a tutorial

--- servoMotorTest.py
+++ servoMotorTest.py
@@ -1,1 +1,22 @@
+from gpiozero import Servo
 from time import sleep
+
+myGPIO = 17
+
+myCorrection = 0.45
+maxPW = (2.0+myCorrection)/1000
+minPW = (1.0-myCorrection)/1000
+
+myServo = Servo(myGPIO, min_pulse_width=minPW, max_pulse_width=maxPW)
+try:
+    while True:
+        myServo.mid()
+        sleep(1)
+        myServo.min()
+        sleep(1)
+        myServo.mid()
+        sleep(1)
+        myServo.max()
+        sleep(1)
+except KeyboardInterrupt:
+    print("Program stopped")
