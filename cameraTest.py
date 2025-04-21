f43009215055518752dd61194a6c94b51eb3255c
Leo Schaefer <leo.schaefer@bluewin.ch>
2025-04-21 12:05:00 +0200

    servo motor angle test
    I made a bunch of photos using a script I made to then later measure the angle of the servo Motor at different duty cycles to make a accurate function converting desired angles into duty cycles

--- cameraTest.py
+++ cameraTest.py
@@ -3,6 +3,20 @@ from time import sleep
 
 picam = Picamera2()
 picam.start_preview(Preview.QTGL)
-picam.start()
-sleep(5)
-picam.close()
+
+picamStarted = False
+
+pictureCount = 0
+while True:
+	try:
+		if not picamStarted:
+			picam.start()
+		
+		com = input("Type camera command: ") 
+		if com.split(" ")[0] == "capture":
+			picam.capture_file("servoAnglePhotos/" + com.split(" ")[1] + ".jpg")
+			
+	except KeyboardInterrupt:
+		picam.close()
+		break
+
