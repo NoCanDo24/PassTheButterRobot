2222590b29a3094a33fac2c6a659c35610b23a6b
Leo Schaefer <leo.schaefer@bluewin.ch>
2025-04-21 10:47:15 +0200

    show middle of object

--- personFollowingTest.py
+++ personFollowingTest.py
@@ -6,14 +6,21 @@ d = objectDetection.objectDetection(trackedObjects=["person"])
 while True:
     try:
         detections, frame = d.getBoxes()
-        cv2.imshow('detection Results', frame)
+
+
 
         if len(detections) > 0:  
             for i in range(len(detections)):
                 positionX = detections[i].xmin + (detections[i].xmax - detections[i].xmin)/2
                 positionY = detections[i].ymin + (detections[i].ymax - detections[i].ymin)/2
+
+                cv2.rectangle(frame, (positionX - 10, positionY + 10), (positionX + 10, positionY - 10), (255,0,0), cv2.FILLED) # Draw white box to put label text in
+
                 
                 print(positionX, positionY)
+
+        cv2.imshow('detection Results', frame)
+
     except KeyboardInterrupt:
         print("Program stopped")
         
