79fdca0504aae5aaa5ee9263b806e13d319b3550
Leo Schaefer <leo.schaefer@bluewin.ch>
2025-05-29 09:46:56 +0200

    New Stepper motor Driver
    I wrote a script that should work with the new Stepper motor driver I bought (TMC2209)

--- stepperMotorDriver.py
+++ stepperMotorDriver.py
@@ -1,50 +1,29 @@
 
-from gpiozero import OutputDevice, PWMOutputDevice
+from gpiozero import OutputDevice
 from time import sleep
+import math
 
 # Pin Definitions
-IN1 = OutputDevice(12)
-IN2 = OutputDevice(16)
-IN3 = OutputDevice(20)
-IN4 = OutputDevice(21)
-ENA = PWMOutputDevice(7)  # ENA for Motor 1
-ENB = PWMOutputDevice(8)  # ENB for Motor 2
+step = OutputDevice(17)
+dir = OutputDevice(27)
 
-# Define step sequence for the motor
-step_sequence = [
-    [1, 1, 0, 0],
-    [0, 1, 1, 0],
-    [0, 0, 1, 1],
-    [1, 0, 0, 1]
-]
+def step(steps):
+    if steps >= 0:
+        dir.value = 1
+    else:
+         dir.value = 0
+    
+    for i in range(abs(steps)):
+            step.value = 1
+            sleep(0.001)
+            step.value = 0
+            sleep(0.001)
 
-reversed_step_sequence = [
-    [1, 0, 0, 1],
-    [0, 0, 1, 1],
-    [0, 1, 1, 0],
-    [1, 1, 0, 0]
-]
-
-def set_step(w1, w2, w3, w4):
-    IN1.value = w1
-    IN2.value = w2
-    IN3.value = w3
-    IN4.value = w4
-
-def step_motor(steps, direction=1, delay=0.01):
-    ENA.value = 1  # Enable Motor 1
-    # ENB.value = 1  # Enable Motor 2
-    for _ in range(steps):
-        for step in (step_sequence if direction > 0 else reversed_step_sequence):
-            set_step(*step)
-            sleep(delay)
-    ENA.value = 0  # Disable Motor 1
-    ENB.value = 0  # Disable Motor 2
 
 try:
     while True:
         steps = 50
         direction = int(input("Enter direction (1 for forward, -1 for backward): "))
-        step_motor(steps, direction)
+        step(steps*math.copysign(1, direction))
 except KeyboardInterrupt:
     print("Program stopped by user")
