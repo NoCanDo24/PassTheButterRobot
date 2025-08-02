67e8a0cf0f543e6bc977748210a58c56e5eff951
Leo Schaefer <leo.schaefer@bluewin.ch>
2025-08-02 13:42:04 +0200

    Hotfix

--- motorEncoderTest.py
+++ motorEncoderTest.py
@@ -5,15 +5,7 @@ right_b = DigitalInputDevice(6)
 left_a = DigitalInputDevice(24)
 left_b = DigitalInputDevice(23)
 
-right_a.when_activated = handle_encoder_right
-right_a.when_deactivated = handle_encoder_right
-right_b.when_activated = handle_encoder_right
-right_b.when_deactivated = handle_encoder_right
 
-left_a.when_activated = handle_encoder
-left_a.when_deactivated = handle_encoder
-left_b.when_activated = handle_encoder
-left_b.when_deactivated = handle_encoder
 
 pulse_count = 0
 
@@ -31,3 +23,13 @@ def handle_encoder_right():
     #         pulse_count += 1
     #     else:
     #         pulse_count.is_active:
+
+right_a.when_activated = handle_encoder_right
+right_a.when_deactivated = handle_encoder_right
+right_b.when_activated = handle_encoder_right
+right_b.when_deactivated = handle_encoder_right
+
+# left_a.when_activated = handle_encoder
+# left_a.when_deactivated = handle_encoder
+# left_b.when_activated = handle_encoder
+# left_b.when_deactivated = handle_encoder
