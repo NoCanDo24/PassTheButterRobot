1e9d84e7acc2eb1e0eb53d301645126e16fe09d3
Leo Schaefer <leo.schaefer@bluewin.ch>
2025-08-02 14:04:19 +0200

    Update motorEncoderTest.py

--- motorEncoderTest.py
+++ motorEncoderTest.py
@@ -7,12 +7,23 @@ left_a = DigitalInputDevice(24)
 left_b = DigitalInputDevice(23)
 
 
-
+old_phase = None
+new_phase = None
 pulse_count = 0
 
 def handle_encoder_right():
 
-    print(f"[{right_a.value}, {right_b.value}]")
+    if old_phase ==  None:
+        old_phase = bin(right_a.value + 2* right_b.value)
+
+    new_phase = bin(right_a.value + 2* right_b.value)
+
+    if old_phase == new_phase >> 1:
+        pulse_count += 1
+    elif old_phase == new_phase << 1:
+        pulse_count -= 1
+
+    print(bin(right_a.value + 2* right_b.value))
 
     # if right_a.is_active:
     #     if right_b.is_active:
@@ -23,7 +34,7 @@ def handle_encoder_right():
     #     if right_b.is_active:
     #         pulse_count += 1
     #     else:
-    #         pulse_count.is_active:
+    #         pulse_count -= 1
 
 right_a.when_activated = handle_encoder_right
 right_a.when_deactivated = handle_encoder_right
